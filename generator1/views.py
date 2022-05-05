de14b62a6419048ff92d4108a8089c88f86bc488
Omar Zhadykov <omar3282088@gmail.com>
2022-05-05 15:58:46 +0300

    COMMIT1

---
+++ generator1/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+# Create your views here.
+
+def home(request):
+	return render(request, 'generator/home.html')
+
+
+def password(request):
+
+	thepassword = 'testing'
+
+	charachters = list('abcdefghijklmnopqrstuvwxyz')
+
+	if request.GET.get('uppercase'):
+		charachters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+	if request.GET.get('numbers'):
+		charachters.extend(list('0123456789'))	
+
+	if request.GET.get('special'):
+		charachters.extend('!@#$%&*?')	
+
+	length = int(request.GET.get('length', 12))
+
+	thepassword = '';
+
+	for x in range(length):
+		thepassword += random.choice(charachters)
+
+	return render(request, 'generator/password.html', {'password' : thepassword})	
+
+
+def about(request):
+	return render(request, 'generator/about.html')	
